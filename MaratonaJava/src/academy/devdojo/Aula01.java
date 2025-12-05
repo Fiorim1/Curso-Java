f7f0d5a3a7e94a8792602523a6db42bb882220b0
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-12-05 02:01:03 -0300

    Add files via upload

---
+++ MaratonaJava/src/academy/devdojo/Aula01.java
@@ -0,0 +1,20 @@
+// Ter o pacote
+package academy.devdojo;
+
+public class Aula01 {
+
+    // Comentário em Linha
+
+    /* Comentário de Múltiplas Linhas */
+
+    /**
+     * Comentário em JavaDoc (Usar para explicar o que é)
+     */
+
+
+    public static void main(String[] args){
+
+        // Imprime Algo
+        System.out.println("Olá Mundo, tudo bem?!");
+    }
+}
---
+++ MaratonaJava/src/academy/devdojo/Aula02.java
@@ -0,0 +1,18 @@
+package academy.devdojo;
+
+public class Aula02 {
+
+    // Tipos Primitivos
+
+    public static void main (String[] args) {
+
+        // São tipos que vão guardat em memória um valor simples
+
+        // int, double, float, char, byte, short, long e boolean
+
+        // Criar Variável = Tipo + Nome =
+        int age = 20;
+        // Imprimindo a variável "age"
+        System.out.println("A idade é " + age  + " anos.");
+    }
+}
