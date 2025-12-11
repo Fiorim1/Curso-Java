1c9f1e7091bbc84d137875ec07e94229d69b38dd
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-12-11 11:41:59 -0300

    Add files via upload

---
+++ Curso-Java-main/MaratonaJava/src/academy/devdojo/Aula07OperadoresLogicos.java
@@ -0,0 +1,15 @@
+package Aulas;
+
+public class Aula07OperadoresLogicos {
+    public static void main(String[] args) {
+        // Operadores Lógicos retornam valores booleanos (verdadeiro - True e falso - False)
+        // Operadores Lógicos = < > <= >=
+
+        boolean isDezMenorQueVinte = 10 < 20;
+        // Imprimindo para ver se 10 é menor que 20
+        System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
+        boolean isTrintaMaiorQueCem = 30 > 100;
+        System.out.println("isTrintaMaiorQueCem " + isTrintaMaiorQueCem);
+
+    }
+}
