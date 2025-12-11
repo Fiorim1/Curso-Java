77c32cee07af075caf7083671a7a7d264e5d0ffb
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-12-11 14:41:20 -0300

    Update Aula07OperadoresLogicos.java

--- Curso-Java-main/MaratonaJava/src/Aulas/Aula07OperadoresLogicos.java
+++ Curso-Java-main/MaratonaJava/src/Aulas/Aula07OperadoresLogicos.java
@@ -3,13 +3,27 @@ package Aulas;
 public class Aula07OperadoresLogicos {
     public static void main(String[] args) {
         // Operadores Lógicos retornam valores booleanos (verdadeiro - True e falso - False)
-        // Operadores Lógicos = < > <= >=
+        // Operadores Lógicos = < > <= >= == !=
 
         boolean isDezMenorQueVinte = 10 < 20;
         // Imprimindo para ver se 10 é menor que 20
         System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
         boolean isTrintaMaiorQueCem = 30 > 100;
         System.out.println("isTrintaMaiorQueCem " + isTrintaMaiorQueCem);
+
+        boolean isQuarentaMaiorOuIgualACinquenta = 40 >= 50;
+        // Imprimindo para ver se 40 é maior ou igual a 50
+        System.out.println(isQuarentaMaiorOuIgualACinquenta);
+
+        boolean isDezMenorOuIgualAVinte = 10 <= 20;
+        System.out.println(isDezMenorOuIgualAVinte);
+
+        boolean igual = 20 == 20.00;
+        // Imprimindo para ver se 20 é igual a 20.00
+        System.out.println(igual);
+
+        boolean diferente = 20 != 20.0001;
+        System.out.println(diferente);
 
     }
 }
