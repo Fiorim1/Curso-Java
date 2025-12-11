40f84ef0cc9eddc7a39057b4d4aa45df07d608bb
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-12-11 10:48:36 -0300

    Delete MaratonaJava directory

--- MaratonaJava/src/academy/devdojo/Aula03String.java
+++
@@ -1,7 +0,0 @@
-package academy.devdojo;
-
-public class Aula03String {
-    // String é uma classe, ou seja, começa com letra maiúscula
-    // Na string usamos aspas duplas ""
-    String nome = "Gabriel Fiorim Esteves";
-}
