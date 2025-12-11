7d10a626f474487b8d1058836ae711ed679947ce
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-12-11 15:18:45 -0300

    Add files via upload

---
+++ Curso-Java-main/MaratonaJava/src/Aulas/Aula10LogicaNegation.java
@@ -0,0 +1,16 @@
+package Aulas;
+
+public class Aula10LogicaNegation {
+    public static void main(String[] args) {
+        // Operadores Lógicos: negação (!)
+        // Sempre retorna valores booleanos os operadores lógicos
+
+        boolean estaSol = true;
+        boolean estaNublado = true;
+
+        boolean naoEstaNublado = !estaSol;
+        System.out.println("Não está sol? " + naoEstaNublado);
+
+
+    }
+}
