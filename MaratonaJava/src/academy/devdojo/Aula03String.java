0e1ce34b2987539fcf78f7f24f7690a5977102c0
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-12-11 10:42:50 -0300

    Add files via upload

---
+++ MaratonaJava/src/academy/devdojo/Aula03String.java
@@ -0,0 +1,7 @@
+package academy.devdojo;
+
+public class Aula03String {
+    // String é uma classe, ou seja, começa com letra maiúscula
+    // Na string usamos aspas duplas ""
+    String nome = "Gabriel Fiorim Esteves";
+}
