cbb7b86eeb70c3b1c894db8f6f432b565132d29d
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-12-11 10:17:16 -0300

    Update Aula02.java

--- MaratonaJava/src/academy/devdojo/Aula02.java
+++ MaratonaJava/src/academy/devdojo/Aula02.java
@@ -6,16 +6,31 @@ public class Aula02 {
 
     public static void main (String[] args) {
 
-        // São tipos que vão guardat em memória um valor simples
+        // São tipos que vão guardar em memória um valor simples
 
         // int, double, float, char, byte, short, long e boolean
 
         // Criar Variável = Tipo + Nome = Valor
-        int age = 20;
+        int idade = 20;
+
+        double salarioDouble = 2000;
+
+        double salarioFloat = 2500;
+
+        byte idadeByte = 10;
+
+        short idadeShot = 10;
+
+        boolean verdsadeiro = true;
+
+        boolean falso = false;
+
+        long numeroGrande = 100000;
+
+        // Usar aspas simples ''
+        char caracter = 'A';
         
         // Imprimindo a variável "age"
-        System.out.println("A idade é " + age  + " anos.");
+        System.out.println("A idade é " + idade  + " anos.");
     }
 }
-
-
